c47d41946a9ee89656431fa846c6551454231995
Zagszy <yuvict1@outlook.com>
2025-05-30 23:45:35 -0700

    updated modinfo to reflect  version of TA

--- modinfo.lua
+++ modinfo.lua
@@ -2,13 +2,13 @@ return {
     name = "Dynamic Robot Defense",
     description = "The New Dynamic Robot Defense",
     shortname = "TADRD",
-    version = "$VERSION",
+    version = "recursive-2312-a70db29",
     mutator = "",
     game = "",
     shortGame = "TA",
     modtype = 1,
     depend = {
-        "Tech Annihilation test-11171-00f7e72",
+        "Tech Annihilation recursive-11634-79c7172",
     },
     NTAI = {
         tdfpath = "TA"
